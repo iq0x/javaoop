70724a0fca03e13529e6107b0a0aee56e3cb6a78
iq0x <88105363+iq0x@users.noreply.github.com>
2021-09-10 10:31:48 +0200

    Update oop_lieferdienst.java

--- oop_lieferdienst.java
+++ oop_lieferdienst.java
@@ -2,48 +2,59 @@ public class paket
 {
     double gewicht;
     double preis;
-
-    double check(double gewicht) 
+    double leange;
+    double tiefe;
+    double breite;
+    
+    double volumen(double leange, double breite, double tiefe)
     {
-		if (gewicht > 20)
-        {
-			System.out.println("Paket kann nicht versendet werden!");
-		}
-		else
-		{
-			if (gewicht >= 5 && gewicht <= 10)
-				preis = 10.50;
-
-			else if (gewicht > 10 && gewicht < 20)
-				preis = 14;
-   
-			else
-				preis = 7;
-		}
-		return preis;
+	return leange * breite * tiefe;
     }
     
-	public static void display(double x) 
+    double kosten(double gewicht) 
     {
-		  System.out.println("Die Kosten betragen " + x + " Euro");
+	if (gewicht > 20)
+        {
+	    System.out.println("Paket kann nicht versendet werden!");
+	}
+	else
+	{
+	    if (gewicht >= 5 && gewicht <= 10)
+		preis = 10.50;
+
+	    else if (gewicht > 10 && gewicht < 20)
+		preis = 14;
+       
+	    else
+		preis = 7;
+	}
+	return preis;
     }
-
-
+    
+    public static void displayKosten(double x) 
+    {
+	System.out.println("Die Kosten betragen " + x + " Euro");
+    }
+    
+     public static void displayVolumen(double x) 
+    {
+	System.out.println("Das Volumen beträgt " + x +"cm³");
+    }
 
     public static void main(String[] args) 
     {
         paket paket = new paket();
 
-        paket.gewicht = 20;
+
+        paket.gewicht = 12;
+	paket.leange = 0.20;
+	paket.breite = 0.20;
+	paket.tiefe = 0.20;
        
-        double kosten = paket.check(paket.gewicht);
+        double kosten = paket.kosten(paket.gewicht);
+	double volumen = paket.volumen(paket.leange, paket.breite, paket.tiefe );
 
-        display(kosten);
-
+        displayKosten(kosten);
+	displayVolumen(volumen);
     }
 }
-
-
-
-
-
