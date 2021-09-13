54f20a5bb15446b8a5528de7c67f4ef34cc58509
iq0x <88105363+iq0x@users.noreply.github.com>
2021-09-13 13:23:23 +0200

    Update oop_mietwagen.java

--- oop_mietwagen.java
+++ oop_mietwagen.java
@@ -1,14 +1,13 @@
 public class mietwagen 
 {
     private String marke;
-
     private double gewicht;
     private double price;
     private double rentprice;
     private String category;
 
 
-	public void setcategory(String name)
+	public void setcategory(String category)
     { 
 		this.category = category;
     }
@@ -29,8 +28,7 @@ public class mietwagen
     }
     
        
-    
-    
+ 
     
     public String getname(String name)
     { 
@@ -49,8 +47,36 @@ public class mietwagen
     
     public String getcategory(String category, double gewicht)
     { 
+		if (gewicht < 1000)
+			category = "kleinwagen";
+		else 
+			category = "grosswagen";
+			
 		return category;
     }
+    
+    
+    
+    public static double kosten(String category, int miettage, int km)
+    {
+		double kosten = 0;
+		if (category.equals("kleinwagen"))
+		{
+			if (km < 200)
+				kosten = miettage * 60 + km * 0.40;
+			else
+				kosten = miettage * 60 + (km - 200) * 0.40 ;
+		}
+		else
+		{
+			if (km < 200)
+				kosten = miettage * 90 + km * 0.60;
+			else
+				kosten = miettage * 90 + (km - 300) * 0.60 ;
+		}
+		
+		return kosten;
+	}
     
 //---------------------------------------------------------------------
 
@@ -60,18 +86,25 @@ public class mietwagen
 		
 	    mietwagen.marke = "audi";
 	    mietwagen.price = 9999.00;
-		mietwagen.gewicht = 1234.00;
+		mietwagen.gewicht = 700.00;
 	    
 	    mietwagen.category = "kleinwagen";
+	    
+	    int miettage = 3;
+	    int km = 200;
 	    
 		clrscr();
 		titel();
 		
-		System.out.print(mietwagen.getname(mietwagen.marke) + "\n");
-		System.out.print(mietwagen.getprice(mietwagen.price) + "\n");
-		System.out.print(mietwagen.getgewicht(mietwagen.gewicht) + "\n");
-		System.out.print(mietwagen.getcategory(mietwagen.category, mietwagen.gewicht) + "\n");
-
+		System.out.print("auto marke:\t\t" + mietwagen.getname(mietwagen.marke) + "\n");
+		System.out.print("auto preis:\t\t" + mietwagen.getprice(mietwagen.price) + "\n");
+		System.out.print("auto gewicht:\t\t" + mietwagen.getgewicht(mietwagen.gewicht) + "\n");
+		System.out.print("auto kategorie:\t\t" + mietwagen.getcategory(mietwagen.category, mietwagen.gewicht) + "\n\n");
+		
+		System.out.println("usereingabe");
+		System.out.println(miettage +" Tage");
+		System.out.println(km +" km");
+		System.out.print(kosten(mietwagen.category, miettage, km) + "\n");
     }
     
 //---------------------------------------------------------------------
