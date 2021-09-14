d736ee7fe4bd9e82521f0141213df3c06751b9fb
iq0x <88105363+iq0x@users.noreply.github.com>
2021-09-14 10:44:57 +0200

    Update oop_urlaubstag.java

--- oop_urlaubstag.java
+++ oop_urlaubstag.java
@@ -1,11 +1,15 @@
+import java.util.regex.*;  
+
 class Main {
   
     public static void main(String args[])
     {
+		clrscr();
         Urlaubstage[] u = new Urlaubstage[5];
   
 		int i = 0;
 		int j = 0;
+		
 		
 		while (i < u.length)
 		{
@@ -13,41 +17,61 @@ class Main {
 			i++;
 		}
        
-		u[0].setData(523451, "Helmut", "Srgiano", "01.05.1945");
-		u[1].setData(756543, "Jan", "Deluxe", "05.03.1975");
-		u[2].setData(123456, "Uwe", "Knecht", "03.09.1999");
-		u[3].setData(423111, "Oliver", "Hammer", "07.11.1990");
-		u[4].setData(987211, "Dieter", "derbear", "01.02.1888");
-		
+		u[0].setData("523451", "Helmut", "Srgiano", "01.05.1945", 30);
+		u[1].setData("756543", "Jan", "Deluxe", "05.03.1975", 30);
+		u[2].setData("123456", "Uwe", "Knecht", "03.09.1999", 30);
+		u[3].setData("423111", "Oliver", "Hammer", "07.11.1990", 30);
+		u[4].setData("987211", "Dieter", "derbear", "01.02.1888", 30);
 		
 		while (j < u.length)
 		{
 			u[j].getData();	
 			j++;
 		}
-		
+    }
+    
+     public static void clrscr()
+    {
+		System.out.print("\033[H\033[2J"); 
+		System.out.flush(); 	
     }
 }
   
 
 class Urlaubstage 
 {  
-    private int id;
+    private String id;
     private String vorname;
     private String nachname;
     private String geb;
+    private int jahresuralub;
     
   
-    public void setData(int id, String vorname, String nachname, String geb)
+    public void setData(String id, String vorname, String nachname, String geb, int jahresuralub)
     {
-        this.id = id;
-        this.vorname = vorname;
-        this.nachname = nachname;
-        this.geb = geb;
+		boolean check;
+		if(check = Pattern.matches("[a-zA-Z0-9]*", id)) 
+			this.id = id;
+		
+		if(check = Pattern.matches("[a-zA-Z]*", vorname)) 
+			this.vorname = vorname;
+			
+		if(check = Pattern.matches("[a-zA-Z]*", nachname))	
+			this.nachname = nachname;
+			
+        if(check = Pattern.matches("[a-zA-Z0-9]*", geb))
+			this.geb = geb;
+			
+
+		this.jahresuralub = jahresuralub;
     }
   
     public void getData()
     {
-        System.out.println(id + "\t" + vorname + "\t" + nachname + "\t" + geb);
+		
+		System.out.println(id + "\t" + vorname + "\t" + nachname + "\t" + geb + "\t" + jahresuralub+"Tage");
+
     }
+    
+  
 }
